fd19ee06c035df741c8cce55ac54265020b2d31a
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2021-09-29 13:16:51 -0400

    added github link in About Page

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -6,6 +6,13 @@ const AboutPage = () => {
       <h3>My contact info:</h3>
       <p className="mb-0">Phone: +1(204)297-9840</p>
       <p>E-mail: illya.korotun@gmail.com.</p>
+      <a
+        href="https://github.com/ILLYAKO"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://github.com/ILLYAKO
+      </a>
     </div>
   );
 };
